b152afe9d6d2031c9b53421a2e41a6e2246aa96b
GregWaldron <92687813+GregWaldron@users.noreply.github.com>
2021-11-30 19:10:05 -0700

    more edits

--- Python/fundamentals/fundamentals/functions_basic_ii.py
+++ Python/fundamentals/fundamentals/functions_basic_ii.py
@@ -3,16 +3,16 @@ def countdown (number):
     for x in range (number, -1, -1):
         list.append(x)
     print (list)
-countdown(15)
+countdown(35)
 
 def print_and_return (list):
     print(list[0])
     return list[1]
-print_and_return ([30,13])
+print_and_return ([11,40])
 
 def first_plus_length (list):
     return (list[0] + len(list))
-print(first_plus_length ([27,30,13,31,17,33]))
+print(first_plus_length ([1,1,2,3,5,8]))
 
 def values_greater_than_second (list):
     newlist = []
@@ -23,11 +23,11 @@ def values_greater_than_second (list):
             newlist.append (list[x])
     print("Number of Values:", len(newlist))
     return newlist
-values_greater_than_second ([27,30,13,31,17,33])
+print(values_greater_than_second ([2,18,3,1,7,98,185]))
 
 def this_length_that_value(size, value):
     newlist=[]
     for x in range (0, size):
         newlist.append(value)
     print(newlist)
-this_length_that_value(10,13)
+this_length_that_value(6, 2)
